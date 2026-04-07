638ea471ea97b0215b201719befed18c969dd846
Ayodele Kehinde <cherrio.llc@gmail.com>
2026-04-07 01:10:22 +0100

    Update server.js

--- result/server.js
+++ result/server.js
@@ -18,7 +18,7 @@ io.on('connection', function (socket) {
 });
 
 var pool = new Pool({
-  connectionString: 'postgresql://app:PGUqVDeX67Mp2yZOJBtQK7XenalQg2AzRIPJsxHydqr2oY0i0mcDwNMDDR7TVRJ6@db:5432/app'
+  connectionString: 'postgresql://app:PGUqVDeX67Mp2yZOJBtQK7XenalQg2AzRIPJsxHydqr2oY0i0mcDwNMDDR7TVRJ6@db-rw.voting.svc.cluster.local:5432/app'
 });
 
 async.retry(
